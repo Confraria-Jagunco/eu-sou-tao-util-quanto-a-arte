00a8ded67cbd98258fa4bd25d70f20a845dba554
Confraria Jagunço <jaguncocomunicationltda@gmail.com>
2025-08-14 16:47:55 -0300

    Js commit

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-const linkHomeHeader = document.getElementById("toHomeHeader");
-const linkHomeFooter = document.getElementById("toHomeFooter");
 
-function toHome () {
-    window.open("https://confraria-jagunco.github.io/a-sensacao-de-conforto-ou-nao-causada-pela-retrospectiva-spotify/");
-};
-
-linkHomeHeader.addEventListener("click", toHome);
-linkHomeFooter.addEventListener("click", toHome);
